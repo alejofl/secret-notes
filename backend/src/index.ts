4f89c23b3c7c3f323afce1f09a2f974930568c73
Alejo Flores Lucey <alejo@misterflores.com>
2025-06-22 01:39:10 +0200

    chore(backend): Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify"
+
+const server = fastify()
+
+server.get("/ping", async (request, reply) => {
+    return "pong\n"
+})
+
+server.listen({
+    host: process.env.BACKEND_HOST || "0.0.0.0",
+    port: +(process.env.BACKEND_PORT || 3000),
+}, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+})
