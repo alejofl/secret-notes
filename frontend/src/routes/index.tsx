b6fa5353d47720aef11996c1ec9157b46c1d554c
Alejo Flores Lucey <alejo@misterflores.com>
2025-07-09 14:18:10 +0200

    feat: Change index

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -7,10 +7,11 @@ export const Route = createFileRoute('/')({
 
 function RouteComponent() {
     return (
-        <div className="grow flex items-center justify-center text-muted-foreground">
+        <div className="grow flex items-center justify-center text-muted-foreground gap-3">
             <div className="flex size-16 items-center justify-center rounded-md bg-muted text-background">
                 <LockKeyhole className="size-12" />
             </div>
+            <span className="text-4xl font-black text-muted">Secret Notes</span>
         </div>
     )
 }
